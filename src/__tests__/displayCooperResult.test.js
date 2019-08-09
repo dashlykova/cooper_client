a997a16340d4f0e9b4afa1f7cda87857d496d868
Daria Lykova <daria.lykova@gmail.com>
2019-08-09 15:40:43 +0200

    Makes display result render the result of the calculation of props/Creates cooper calculator module for displaying the logic behind the calculation

--- src/__tests__/displayCooperResult.test.js
+++ src/__tests__/displayCooperResult.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {shallow} from 'enzyme-adapter-react-16';
-import DisplayCooperResult from '../Components/DisplayCooperResult';
+import {shallow} from 'enzyme';
+import DisplayCooperResult from '../Components/DisplayCooperResult.js';
 
 describe('<DisplayCooperResult>',()=> {
   it('evaluates the correct result for female/poor',() => {
-    const describedComponent = shallow(<DisplayCooperResult distance="1000" gender="female" age="23"/>)
-    const Result = <p>Result: Poor</p>
+    const describedComponent = shallow(<DisplayCooperResult distance="1000" gender="female" age="23"/>);
+    const response = <p>Result: Poor</p>
     expect(describedComponent.contains(response)).toEqual(true)
+    // expect(describedComponent.contains(response)).toEqual(true)
   })
 
-  it('evaluates the correct result for female/avarage',() => {
-    const describedComponent = shallow(<DisplayCooperResult distance="1000" gender="female" age="23"/>)
-    const Result = <p>Result: Avarage</p>
+  it('evaluates the correct result for female/average',() => {
+    const describedComponent = shallow(<DisplayCooperResult distance="2000" gender="female" age="23"/>)
+    const response = <p>Result: Average</p>
     expect(describedComponent.contains(response)).toEqual(true)
   })
 
